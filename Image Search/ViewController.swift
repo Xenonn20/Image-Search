b1cf9849e7c25c45dad34257f692a21a14a3aeb6
Kirill Medvedev <Medvedev-kv@list.ru>
2019-09-16 20:01:45 +0400

    Initial Commit

---
+++ Image Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image Search
+//
+//  Created by Кирилл Медведев on 16/09/2019.
+//  Copyright © 2019 Kirill Medvedev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
